a21754cf750438a08cc173c47a005af7001607cc
Yogendra <yogendraburkul0513@gmail.com>
2022-09-06 17:54:21 +0530

    done

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -14,23 +14,21 @@ public class LoginPageTest extends BaseClass{
 	LoginPage loginPage;
 	HomePage homePage;
 	
-	public LoginPageTest(){
-		super();
-	}
+//	public LoginPageTest(){
+//		super();
+//	}
 	
 	@BeforeMethod
 	public void setUp(){
 		initialization();
 		loginPage = new LoginPage();	
 	}
+
 	
-//	@Parameters("Browser")			//this parameter will be taken from testing.xml --- but we are running with maven cmd -mvn clean test -DBrowser=chrome  
+	//jenkins practice time created this method to test or check how to send using pom in jenkin     
+	@Parameters("Browser")     //this parameter will be taken from testing.xml --- but we are running with maven cmd -mvn clean test -DBrowser=chrome
 	@Test(priority=1)
-	public void loginPageTitleTest(String BrowserName){
-		String title = loginPage.validateLoginPageTitle();
-		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
-//---------------------------------------------------------------------------------------------------------------------------
-		
+	public void loginPageVerify(String BrowserName) {
 		System.out.println("parameter value :"+BrowserName);    //pratice sending parameter using pom file through jenkins ---
 		if(BrowserName.contains("chrome")) {
 			System.out.println("chrome running ");
@@ -38,15 +36,24 @@ public class LoginPageTest extends BaseClass{
 		else {
 			System.out.println("firfox running ");
 		}
-	} 
+		
+	}
+	
 	
 	@Test(priority=2)
+	public void loginPageTitleTest(){			
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
+		
+	} 
+	
+	@Test(priority=3)
 	public void crmLogoImageTest() throws InterruptedException{
 		boolean flag = loginPage.validateCRMImage();
 		Assert.assertTrue(flag);
 	}
 	
-	@Test(priority=3)
+	@Test(priority=4)
 	public void loginTest(){
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
